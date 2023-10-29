3fc7d99ca57602ebbc17daef8186289d76245b09
aminafarah <amishkamohamed563@gmail.com>
2023-10-29 11:51:34 +0300

    passeed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [imageDog, setImageDog] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Inside the useEffect, send a fetch request to the API
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setImageDog(data.message);
+        setIsLoaded(true);
+      });
+  }, []); // Empty dependency array to run the effect once on component mount
+
+  return (
+    <div>
+      {isLoaded ? (
+        <img src={imageDog} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
